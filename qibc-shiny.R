f0f55d06d62592f75549f053887c9fbe4d0a0c97
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2019-12-27 21:54:36 +0000

    responsive slider that shows both min, max and updates when condition is changed

--- qibc-shiny.R
+++ qibc-shiny.R
@@ -22,9 +22,10 @@ ui <- fluidPage(
     column(6,
            selectInput('x', 'X', choices = nms, selected = x.axis, width = '100%'),
            selectInput('meta', 'Condition Name', choices = condition_names),
-           sliderInput('xlim', 'X min/max', min = 1, max = 1e7, value = 1e7)),
+           sliderInput('xlim', 'X min/max', min = 1, max = 1e7, value = c(1, 1e7))),
     column(6, 
-           selectInput('y', 'Y', choices = nms, selected = y.axis, width = '100%'))
+           selectInput('y', 'Y', choices = nms, selected = y.axis, width = '100%'),
+           numericInput('xmin', 'x min', value = 10, min = 1, max = 10))
   )
 
 )
@@ -34,8 +35,11 @@ server <- function(input, output, session) {
   ## Updating a slider based on condition selected
   observe({
     val <- input$x
-    updateSliderInput(session, 'xlim', value = val, 
-                      min = round(max(input_data[get(metadata) == input$meta][[input$x]])))
+    updateSliderInput(session, 'xlim', value = c(round(min(input_data[get(metadata) == input$meta][[input$x]])),
+                                                 round(max(input_data[get(metadata) == input$meta][[input$x]]))), 
+                      min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
+                      max = round(max(input_data[get(metadata) == input$meta][[input$x]]))
+    )
   })
   
   dataset <- reactive({
