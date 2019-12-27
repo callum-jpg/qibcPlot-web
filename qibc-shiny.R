85d3ee3b6c5342306517095b20a4b71434a193dc
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2019-12-27 17:09:26 +0000

    added feature to select specific condition

--- qibc-shiny.R
+++ qibc-shiny.R
@@ -6,19 +6,25 @@ library(data.table)
 input_file <- 'example_data/Nuclei.csv'
 x.axis <- 'Intensity_IntegratedIntensity_rescaledapichannel'
 y.axis <-  'Intensity_MeanIntensity_rescalecy5channel'
+metadata <- 'Metadata_FolderName'
 
 input_data <- fread(input_file, stringsAsFactors = TRUE)
 
 input_data <- as.data.frame(input_data)
 
 nms <- names(input_data)
+condition_names <- levels(input_data[, get(metadata)])
 
 ui <- fluidPage(
   titlePanel('QIBC'),
-  plotlyOutput('qibcPlot', height = '400px', width = '400px'),
+  fluidRow(
+    column(12, align = 'center', plotlyOutput('qibcPlot', height = '500px', width = '500px'))
+    ),
   fluidRow(
     column(6,
-           selectInput('x', 'X', choices = nms, selected = x.axis, width = '100%')),
+           selectInput('x', 'X', choices = nms, selected = x.axis, width = '100%'),
+           selectInput('meta', 'Condition Name', choices = condition_names),
+           sliderInput('xlim', 'X min/max', min = 1, max = 1e7, value = 1000)),
     column(6, 
            selectInput('y', 'Y', choices = nms, selected = y.axis, width = '100%'))
   )
@@ -26,8 +32,13 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  
+  dataset <- reactive({
+    input_data[get(metadata) == input$meta]
+  })
+  
   output$qibcPlot <- renderPlotly({
-    plot <- ggplot(input_data[Metadata_FolderName == 'dt-0.1ug'], 
+    plot <- ggplot(dataset(), 
                    aes_string(x = input$x, 
                               y = input$y)) +
       geom_point(shape = 21, size = 3, colour = "#aaaaaa", stroke = 0.5)
