fd38da19f4617b62bc47ef86b5135ee01298efb1
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2019-12-28 15:15:23 +0000

    added yaxis filter

--- qibc-shiny.R
+++ qibc-shiny.R
@@ -27,6 +27,7 @@ ui <- fluidPage(
     column(6, 
            selectInput('y', 'Y', choices = nms, selected = y.axis, width = '100%'),
            selectInput('colour', 'Point colour', choices = nms, selected = point.colour, width = '100%'),
+           sliderInput('ylim', 'y min/max', min = 1, max = 1e7, value = c(1, 1e7)),
            numericInput('xmin', 'x min', value = 1),
            textOutput('test_print'))
   )
@@ -39,15 +40,25 @@ server <- function(input, output, session) {
     paste('print test:', input$x)
   })
   
-  ## Updating a slider based on condition selected
+  
   observe({
-    val <- input$x
+    ## Updating a xlim slider based on condition selected
     updateSliderInput(session, 'xlim', value = c(round(min(input_data[get(metadata) == input$meta][[input$x]])),
                                                  round(max(input_data[get(metadata) == input$meta][[input$x]]))), 
                       min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
                       max = round(max(input_data[get(metadata) == input$meta][[input$x]]))
     )
   })
+  
+  observe({
+    ## Updating a xlim slider based on condition selected
+    updateSliderInput(session, 'ylim', value = c(round(min(input_data[get(metadata) == input$meta][[input$y]])),
+                                                 round(max(input_data[get(metadata) == input$meta][[input$y]]))), 
+                      min = round(min(input_data[get(metadata) == input$meta][[input$y]])),
+                      max = round(max(input_data[get(metadata) == input$meta][[input$y]]))
+    )
+  })
+  
   
   observe({
     # For updating numeric field
@@ -58,7 +69,8 @@ server <- function(input, output, session) {
   
   dataset <- reactive({
     input_data[get(metadata) == input$meta
-    & get(input$x) %inrange% input$xlim] # Filtering data based on x-axis
+    & get(input$x) %inrange% input$xlim
+    & get(input$y) %inrange% input$ylim] # Filtering data based on x-axis
   })
   
   output$qibcPlot <- renderPlotly({
