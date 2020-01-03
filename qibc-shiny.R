3f5d8f06964b39b0413a0a05511188aa497f1716
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-01-03 14:28:52 +0000

    Added metadata filter. Broke updating x/y axis

--- qibc-shiny.R
+++ qibc-shiny.R
@@ -12,6 +12,7 @@ metadata <- 'Metadata_FolderName'
 input_data <- fread(input_file, stringsAsFactors = TRUE)
 
 nms <- names(input_data)
+meta.nms <- nms[grepl('Meta', nms)]
 condition_names <- levels(input_data[, get(metadata)])
 trans <- c('linear', 'log10')
 
@@ -23,11 +24,16 @@ ui <- fluidPage(
   fluidRow(
     column(6,
            selectInput('x', 'X', choices = nms, selected = x.axis, width = '100%'),
-           selectInput('meta', 'Condition Name', choices = condition_names),
+           #br(),
+           h3('use metadata?'),
+           checkboxInput('meta_check', 'Use metadata?', FALSE, width = '100%'),
+           #br(),
+           selectInput('meta', 'Condition Name', ""),
            sliderInput('xlim', 'X min/max', min = 1, max = 1e7, value = c(1, 1e7), width = '100%'),
            selectInput('colour', 'Point colour', choices = nms, selected = point.colour, width = '100%')),
     column(6, 
            selectInput('y', 'Y', choices = nms, selected = y.axis, width = '100%'),
+           selectInput('meta_col', 'Metadata Column', "", width = "100%"),
            selectInput('ytrans', 'y transformation', choices = trans, selected = 'log10', width = '100%'),
            sliderInput('ylim', 'y min/max', min = 1, max = 1e7, value = c(1, 1e7), width = '100%'),
            sliderInput('colour_lim', 'colour min/max', min = 1, max = 3000, value = c(1, 100), width = '100%'),
@@ -40,42 +46,57 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   output$test_print <- renderText ({
-    paste('print test:', length(dataset()))
+    paste('print test:', input$meta_col == input$meta)
   })
   observe({
-    ## Updating a xlim slider based on condition selected
-    updateSliderInput(session, 'xlim', value = c(round(min(input_data[get(metadata) == input$meta][[input$x]])),
-                                                 round(max(input_data[get(metadata) == input$meta][[input$x]]))), 
-                      min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
-                      max = round(max(input_data[get(metadata) == input$meta][[input$x]]))
-    )
-  })
-  observe({
-    ## Updating a xlim slider based on condition selected
-    updateSliderInput(session, 'ylim', value = c(round(min(input_data[get(metadata) == input$meta][[input$y]])),
-                                                 round(max(input_data[get(metadata) == input$meta][[input$y]]))), 
-                      min = round(min(input_data[get(metadata) == input$meta][[input$y]])),
-                      max = round(max(input_data[get(metadata) == input$meta][[input$y]]))
-    )
-  })
-  observe({
-    ## Updating a point colour limit slider based on condition selected
-    updateSliderInput(session, 'colour_lim', value = c(round(min(input_data[get(metadata) == input$meta][[input$colour]])),
-                                                 round(max(input_data[get(metadata) == input$meta][[input$colour]]))), 
-                      min = round(min(input_data[get(metadata) == input$meta][[input$colour]])),
-                      max = round(max(input_data[get(metadata) == input$meta][[input$colour]]))
-    )
-  })
-  observe({
-    # For updating numeric field
-    updateNumericInput(session, 'xmin', value = round(min(input_data[get(metadata) == input$meta][[input$x]])),
-                       min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
-                       max = round(max(input_data[get(metadata) == input$meta][[input$x]])))
+    ## Update meta_col with column names containing Metadata if meta_check is TRUE
+    {if(input$meta_check == TRUE)updateSelectInput(session, 'meta_col' , choices = meta.nms)
+      else updateSelectInput(session, 'meta_col', choices = "")}
   })
   
+  observe({
+    ## Update meta_col with column names containing Metadata if meta_check is TRUE
+    {if(nchar(input$meta_col) >= 1)updateSelectInput(session, 'meta' , choices = levels(input_data[,get(input$meta_col)]))
+      else updateSelectInput(session, 'meta' , choices = "")}
+  })
+  
+  # observe({
+  #   ## Updating a xlim slider based on condition selected
+  #   updateSliderInput(session, 'xlim', value = c(round(min(input_data[get(metadata) == input$meta][[input$x]])),
+  #                                                round(max(input_data[get(metadata) == input$meta][[input$x]]))), 
+  #                     min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
+  #                     max = round(max(input_data[get(metadata) == input$meta][[input$x]]))
+  #   )
+  # })
+  # observe({
+  #   ## Updating a xlim slider based on condition selected
+  #   updateSliderInput(session, 'ylim', value = c(round(min(input_data[get(metadata) == input$meta][[input$y]])),
+  #                                                round(max(input_data[get(metadata) == input$meta][[input$y]]))), 
+  #                     min = round(min(input_data[get(metadata) == input$meta][[input$y]])),
+  #                     max = round(max(input_data[get(metadata) == input$meta][[input$y]]))
+  #   )
+  # })
+  # observe({
+  #   ## Updating a point colour limit slider based on condition selected
+  #   updateSliderInput(session, 'colour_lim', value = c(round(min(input_data[get(metadata) == input$meta][[input$colour]])),
+  #                                                round(max(input_data[get(metadata) == input$meta][[input$colour]]))), 
+  #                     min = round(min(input_data[get(metadata) == input$meta][[input$colour]])),
+  #                     max = round(max(input_data[get(metadata) == input$meta][[input$colour]]))
+  #   )
+  # })
+  # observe({
+  #   # For updating numeric field
+  #   updateNumericInput(session, 'xmin', value = round(min(input_data[get(metadata) == input$meta][[input$x]])),
+  #                      min = round(min(input_data[get(metadata) == input$meta][[input$x]])),
+  #                      max = round(max(input_data[get(metadata) == input$meta][[input$x]])))
+  # })
+  # 
   dataset <- reactive({
     input_data[
-      get(metadata) == input$meta
+      # If metadata is selected, filter dataset based on metadata values
+      {if(nchar(input$meta) >= 1)get(input$meta_col) == input$meta
+      # else, plot all available data
+      else '' %like% ''}
     & get(input$x) %inrange% input$xlim
     & get(input$y) %inrange% input$ylim
     & get(input$colour) %inrange% input$colour_lim
@@ -87,8 +108,7 @@ server <- function(input, output, session) {
                    aes_string(x = input$x, 
                               y = input$y)) +
       # First geom_point added to give second geom_points an outline
-      geom_point(shape = 21, size = 2, stroke = 0.5, colour = "#aaaaaa",
-                 aes_string(input$x, input$y)) +
+      geom_point(shape = 21, size = 2, stroke = 0.5, colour = "#aaaaaa") +
       # Second geom_point
       geom_point(shape = 21, size = 1, 
                  #colour = "#aaaaaa", 
@@ -103,8 +123,6 @@ server <- function(input, output, session) {
       {if(input$ytrans == 'log10')scale_y_log10()} + # Select ylog10
       {if(input$ytrans == 'linear')scale_y_continuous()} # Select y continuous
   })
-  
-    
 }
 
 
