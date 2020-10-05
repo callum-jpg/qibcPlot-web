950346e2c68704572ba058e71f7aafa4767cbb41
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2020-10-05 15:12:27 +0100

    removed non-intensity columns from being selected

--- qibc-shiny.R
+++ qibc-shiny.R
@@ -13,12 +13,12 @@ y.axis <-  'Mean_PCNA_Intensity'
 point.colour <- 'Mean_γH2AX_intensity'
 metadata <- 'Condition'
 
+
 input_data <- fread(input_file, stringsAsFactors = TRUE)
 
 nms <- names(input_data)
-#meta.nms <- nms[grepl('Meta', nms)]
-meta.nms <- nms
-condition_names <- levels(input_data[, get(metadata)])
+intensity_values <- nms[!nms %in% c('V1', 'Condition')] # Remove V1 and Condition from selectable plotting columns
+condition_names <- levels(input_data[, get(metadata)]) # Extract condition names from metadata column
 trans <- c('linear', 'log10')
 
 # Plotting colours
@@ -33,15 +33,15 @@ ui <- fluidPage(
   ),
   fluidRow(
     column(6,
-           selectInput('x', 'X axis', choices = nms, selected = x.axis, width = '100%'),
+           selectInput('x', 'X axis', choices = intensity_values, selected = x.axis, width = '100%'),
            selectInput('meta', 'Condition Name', choices = condition_names, width = "100%"),
            sliderInput('xlim', 'X min/max', min = 1, max = 1e7, value = c(1, 1e7), width = '100%'),
            sliderInput('colour_lim', 'colour min/max', min = 1, max = 3000, value = c(1, 100), width = '100%'),
            br()),
            
     column(6, 
-           selectInput('y', 'Y axis', choices = nms, selected = y.axis, width = '100%'),
-           selectInput('colour', 'Point colour', choices = nms, selected = point.colour, width = '100%'),
+           selectInput('y', 'Y axis', choices = intensity_values, selected = y.axis, width = '100%'),
+           selectInput('colour', 'Point colour', choices = intensity_values, selected = point.colour, width = '100%'),
            sliderInput('ylim', 'y min/max', min = 1, max = 1e7, value = c(1, 1e7), width = '100%'),
            selectInput('ytrans', 'y transformation', choices = trans, selected = 'log10', width = '100%'),
            br()),
